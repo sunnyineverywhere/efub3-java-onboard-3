a647ec1450f145340ab9b225eba4da3babbdb762
Min A <min2010011@gmail.com>
2023-03-31 16:21:15 +0900

    feat: account 도메인 & Lecture 클래스 정의

---
+++ src/main/java/seminar/account/Person.java
@@ -0,0 +1,49 @@
+package seminar.account;
+
+public class Person {
+    private String userId;
+    private String name;
+    private String college;
+    private String department;
+    private String major;
+
+    public String getId() {
+        return userId;
+    }
+
+    public void setId(String id) {
+        this.userId = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
--- src/main/java/seminar/account/Professor.java
+++ src/main/java/seminar/account/Professor.java
@@ -1,4 +1,24 @@
 package seminar.account;
 
-public class Professor {
+public class Professor extends Person {
+    private String professorId;
+    private Status status;
+
+    @Override
+    public String getId() {
+        return professorId;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.professorId = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
