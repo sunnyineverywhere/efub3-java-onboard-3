02760984efdf14fa6634d95098f964132656075c
YEJI <yeji.lee.kim@gmail.com>
2023-04-05 12:50:58 +0900

    feat: Student 작성

--- src/main/java/seminar/account/Student.java
+++ src/main/java/seminar/account/Student.java
@@ -1,5 +1,27 @@
 package seminar.account;
 
-public class Student {
+public class Student extends Account{
+    String studentId;
+    Grade grade;
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getMajor(){
+        return this.major;
+    }
+
+    public Grade getGrade() {
+        return this.grade;
+    }
 
 }
