685a63bef5eba8e9347a2d44322f05d334cd453c
YEJI <yeji.lee.kim@gmail.com>
2023-04-05 12:50:36 +0900

    feat: Account 작성

--- src/main/java/seminar/account/Account.java
+++ src/main/java/seminar/account/Account.java
@@ -1,4 +1,26 @@
 package seminar.account;
 
 public class Account {
+    String userId;
+    String name;
+    String college;
+    String department;
+    String major;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
 }
