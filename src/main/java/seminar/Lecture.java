3850bfd2d6bc6b85d00af5b90ce89a705c7e694e
YEJI <yeji.lee.kim@gmail.com>
2023-04-05 12:52:43 +0900

    feat: Lecture 작성

--- src/main/java/seminar/Lecture.java
+++ src/main/java/seminar/Lecture.java
@@ -1,5 +1,29 @@
 package seminar;
 
 public class Lecture {
+    private String lectureId;
+    private String major;
+    private String name;
+    private String professorId;
+    private Integer total;
 
+    public void setLectureId(String lectureId) {
+        this.lectureId = lectureId;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfessorId(String professorId) {
+        this.professorId = professorId;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
 }
