5d00e44548fb82efb73ee2eb2f1a7906832985ba
YEJI <yeji.lee.kim@gmail.com>
2023-04-05 12:51:29 +0900

    feat: Professor 작성

--- src/main/java/seminar/account/Professor.java
+++ src/main/java/seminar/account/Professor.java
@@ -1,4 +1,30 @@
 package seminar.account;
 
-public class Professor {
+public class Professor extends Account{
+    String professorId;
+    Status status;
+
+    public void setProfessorId(String professorId) {
+        this.professorId = professorId;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setMajor(String major){
+        this.major = major;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getMajor(){
+        return this.major;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
 }
